8895f99edde738ec70f90aa759ce0b34173a93be
liam <daydayup2012@gmail.com>
2015-01-06 17:56:34 +0800

    添加文件

---
+++ clientlib_4.0/MemCachedTest_4.0/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MemCachedTest_4._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
